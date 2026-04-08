6886d55ea798e3ca8bf56c262ff25c1d127ce53a
Luis Machado <boostperformancedigitalltda@gmail.com>
2026-04-08 18:53:11 -0300

    debug: enable production source maps to identify TDZ error

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['firebase'],
-  webpack: (config, { dev, isServer }) => {
-    // Disable module concatenation (scope hoisting) in production client build
-    // to prevent "Cannot access X before initialization" TDZ errors on iOS WebKit
-    if (!dev && !isServer) {
-      config.optimization.concatenateModules = false
-    }
-    return config
-  },
+  productionBrowserSourceMaps: true,
 }
 
 module.exports = nextConfig
